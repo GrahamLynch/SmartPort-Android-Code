df4c83e3ba9ba5d92cda4414dd441ae714442668
Graham Lynch <grahamlynch10@hotmail.com>
2020-04-27 13:05:04 +0100

    Main activity onCreate unit test

--- SmartPort/app/src/androidTest/java/com/example/smartport/MainActivityTest.java
+++ SmartPort/app/src/androidTest/java/com/example/smartport/MainActivityTest.java
@@ -1,16 +1,50 @@
 package com.example.smartport;
 
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import androidx.test.rule.ActivityTestRule;
 
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void onCreate() {
+    private MainActivity mActivity = null ;
+    @Before
+    public void setUp() throws Exception{
+        mActivity = mActivityTestRule.getActivity();
     }
 
     @Test
+    public void onCreate() {
+        View selectFlightView = mActivity.findViewById(R.id.selectFlightTv);
+        View viewFlightInfoView = mActivity.findViewById(R.id.viewFlightInfoTv);
+        View pointsOfInterestView = mActivity.findViewById(R.id.pointsOfInterestTv);
+        View scheduleView = mActivity.findViewById(R.id.scedhuleTv);
+        View profileView = mActivity.findViewById(R.id.profileTv);
+        View logoutView = mActivity.findViewById(R.id.logoutTv);
+        assertNotNull(selectFlightView);
+        assertNotNull(viewFlightInfoView);
+        assertNotNull(pointsOfInterestView);
+        assertNotNull(scheduleView);
+        assertNotNull(profileView);
+        assertNotNull(logoutView);
+    }
+    /*
+    @Test
     public void onClick() {
+    }
+
+     */
+
+    @After
+    public void tearDown() throws Exception{
+        mActivity = null;
     }
 }

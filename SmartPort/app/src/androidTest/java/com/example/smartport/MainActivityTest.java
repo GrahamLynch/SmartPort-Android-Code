fcb9b65677c48468a4b75481e34b72a7847fa111
Graham Lynch <grahamlynch10@hotmail.com>
2020-04-27 15:38:17 +0100

    Main activity testing

--- SmartPort/app/src/androidTest/java/com/example/smartport/MainActivityTest.java
+++ SmartPort/app/src/androidTest/java/com/example/smartport/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.example.smartport;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.view.View;
 
 import org.junit.After;
@@ -9,6 +11,10 @@ import org.junit.Test;
 
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
@@ -16,6 +22,14 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     private MainActivity mActivity = null ;
+
+    Instrumentation.ActivityMonitor flightsActivityMonitor = getInstrumentation().addMonitor(FlightsActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor flightsInfoActivityMonitor = getInstrumentation().addMonitor(FlightInformationActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor pointsOfInterestActivityMonitor = getInstrumentation().addMonitor(PointsOfInterestActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor scheduleActivityMonitor = getInstrumentation().addMonitor(ScheduleActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor userProfileActivityMonitor = getInstrumentation().addMonitor(UserProfileActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor loginActivityMonitor = getInstrumentation().addMonitor(LoginActivity.class.getName(),null,false);
+    //yourFlightInformation
     @Before
     public void setUp() throws Exception{
         mActivity = mActivityTestRule.getActivity();
@@ -36,12 +50,28 @@ public class MainActivityTest {
         assertNotNull(profileView);
         assertNotNull(logoutView);
     }
-    /*
+
+
+
     @Test
     public void onClick() {
-    }
+        assertNotNull(mActivity.findViewById(R.id.yourFlights));
+        assertNotNull(mActivity.findViewById(R.id.yourFlightInformation));
+        assertNotNull(mActivity.findViewById(R.id.pointsOfInterest));
+        assertNotNull(mActivity.findViewById(R.id.scedhule));
+        assertNotNull(mActivity.findViewById(R.id.userProfile));
+        assertNotNull(mActivity.findViewById(R.id.logout));
 
-     */
+        onView(withId(R.id.yourFlights)).perform(click());
+        Activity flightsActivity =  getInstrumentation().waitForMonitorWithTimeout(flightsActivityMonitor,5000);
+        assertNotNull(flightsActivity);
+        flightsActivity.finish();
+
+
+
+
+
+    }
 
     @After
     public void tearDown() throws Exception{
